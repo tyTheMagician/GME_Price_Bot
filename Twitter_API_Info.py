1b327a2fbe5d9aec35d66c6f053f5e9e47af887e
halfdane <halfdane@gmx.net>
2021-09-07 09:36:18 +0200

    introduce server and collaborator friendly secrets handling

---
+++ Twitter_API_Info.py
@@ -0,0 +1,5 @@
+import os
+
+T_API_KEY = os.getenv("twitter_apikey", "some twitter_apikey test value")
+T_KEY_SECRET = os.getenv("twitter_apisecret", "some twitter_apisecret test value")
+T_BEARER_TOKEN = os.getenv("twitter_bearer_token", "some twitter_bearer_token test value")
