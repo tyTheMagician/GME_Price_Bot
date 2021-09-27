b7bb2a183fb12939d2b5dc52aeb71e4f745af48f
ty <kotlowski.tyler@icloud.com>
2021-09-27 02:16:20 -0400

    ocd - quotes

--- TwelveData_API.py
+++ TwelveData_API.py
@@ -3,4 +3,4 @@ import os
 
 # 1) get API --> https://twelvedata.com/
 # 2) set OS variables locally
-US_STOCK_API = os.getenv("us_stock_api", "some twelvedatapikey test value")
+US_STOCK_API = os.getenv('us_stock_api', 'some twelvedatapikey test value')
